d6f3620c239dcda046f1163e11b8f2ecf539aded
Tanmay Ingole <tanmay.ingole@engi.pl>
2021-09-05 16:46:55 +0200

    changed css for responsive view

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Quantity {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -11,5 +14,20 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  selectedValue: string;
+  selectedCar: string;
+
+  quantities: Quantity[] = [
+    { value: '1', viewValue: '1' },
+    { value: '2', viewValue: '2' },
+    { value: '3', viewValue: '3' },
+    { value: '4', viewValue: '4' },
+    { value: '5', viewValue: '5' },
+    { value: '6', viewValue: '6' },
+    { value: '7', viewValue: '7' },
+    { value: '8', viewValue: '8' },
+    { value: '9', viewValue: '9' },
+    { value: '10', viewValue: '10' },
+  ];
 
 }
